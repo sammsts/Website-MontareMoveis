adc5e00a0a42e34571d57589d1638118a3e7773f
sammsts <samuelmacielsts@gmail.com>
2024-03-26 22:26:12 -0300

    Feat: Creating a gallery to display projects

--- src/ui/components/instafeed/InstaFeed.jsx
+++ src/ui/components/instafeed/InstaFeed.jsx
@@ -1,8 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Galleria } from 'primereact/galleria';
+import { Carousel } from 'primereact/carousel';
 
 const InstaFeed = () => {
     const [feedList, setFeedList] = useState([]);
+    const responsiveOptions = [
+        {
+          breakpoint: '991px',
+          numVisible: 4,
+        },
+        {
+          breakpoint: '767px',
+          numVisible: 3,
+        },
+        {
+          breakpoint: '575px',
+          numVisible: 1,
+        },
+      ];
 
     useEffect(() => {
         const getInstaFeed = async () => {
@@ -13,7 +29,6 @@ const InstaFeed = () => {
             try {
                 const { data } = await axios.get(url);
                 setFeedList(data.data);
-                console.log(data);
             } catch (error) {
                 console.error("Error fetching Instagram feed:", error);
             }
@@ -22,17 +37,40 @@ const InstaFeed = () => {
         getInstaFeed();
     }, []);
 
+    const itemTemplate = (item) => {
+        if(item.media_type === "IMAGE"){
+            return(
+                <a key={item.id} href={item.permalink} target="_blank" rel="noreferrer" className="item rounded overflow-hidden transition duration-400 ease-in-out hover:brightness-130">
+                    <img src={item.media_url} style={{ width: '100%' }} />
+                </a>
+                );
+        } 
+        else if (item.media_type === "CAROUSEL_ALBUM") {
+            return (
+                <a key={item.id} href={item.permalink} target="_blank" rel="noreferrer" className="item rounded overflow-hidden transition duration-400 ease-in-out hover:brightness-130">
+                    <img src={item.media_url} style={{ width: '100%' }} />
+                </a>
+            );
+        } 
+        else {
+            return(
+                <a key={item.id} href={item.permalink} target="_blank" rel="noreferrer" className="item rounded overflow-hidden transition duration-400 ease-in-out hover:brightness-130">
+                    <video controls className="w-full h-full">
+                        <source src={item.media_url}></source>
+                    </video>
+                </a>
+            )
+        }
+    };
+    
+    const thumbnailTemplate = (item) => {
+        return <img src={item.media_url} style={{ width: '40%' }} />;
+    };
+
     return (
-        <section>
-            <p>INSTA FEED</p>
-            <ul>
-                {feedList.map((item) => (
-                    <li key={item.id}>
-                        <img src={item.media_url} alt="Instagram post" />
-                        <a href={item.permalink}>View on Instagram</a>
-                    </li>
-                ))}
-            </ul>
+        <section className="container mx-auto grid grid-cols-auto-fit gap-6 max-w-screen-lg md:max-w-screen-xl lg:max-w-screen-2xl px-4 md:px-6 lg:px-8 py-4 md:py-6 lg:py-8">
+            <Galleria value={feedList} responsiveOptions={responsiveOptions} numVisible={5} style={{ maxWidth: '640px' }} 
+            item={itemTemplate} thumbnail={thumbnailTemplate} />
         </section>
     );
 };
