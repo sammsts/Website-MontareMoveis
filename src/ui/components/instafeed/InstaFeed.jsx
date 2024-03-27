39833a0b360730f95dd324f7e37bc98a4bf32b3c
Samuel Maciel <samuelmacielsts@gmail.com>
2024-03-27 08:50:41 -0300

    Fix: Adjusting gallery image size

--- src/ui/components/instafeed/InstaFeed.jsx
+++ src/ui/components/instafeed/InstaFeed.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Galleria } from 'primereact/galleria';
-import { Carousel } from 'primereact/carousel';
 
 const InstaFeed = () => {
     const [feedList, setFeedList] = useState([]);
@@ -23,7 +22,7 @@ const InstaFeed = () => {
     useEffect(() => {
         const getInstaFeed = async () => {
             const token = process.env.REACT_APP_INSTA_FEED_TOKEN;
-            const fields = "media_url, media_type, permalink";
+            const fields = "caption, media_url, media_type, permalink, thumbnail_url";
             const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}`;
             
             try {
@@ -41,14 +40,14 @@ const InstaFeed = () => {
         if(item.media_type === "IMAGE"){
             return(
                 <a key={item.id} href={item.permalink} target="_blank" rel="noreferrer" className="item rounded overflow-hidden transition duration-400 ease-in-out hover:brightness-130">
-                    <img src={item.media_url} style={{ width: '100%' }} />
+                    <img src={item.media_url} style={{ width: '100%' }} alt={item.caption} />
                 </a>
                 );
         } 
         else if (item.media_type === "CAROUSEL_ALBUM") {
             return (
                 <a key={item.id} href={item.permalink} target="_blank" rel="noreferrer" className="item rounded overflow-hidden transition duration-400 ease-in-out hover:brightness-130">
-                    <img src={item.media_url} style={{ width: '100%' }} />
+                    <img src={item.media_url} style={{ width: '100%' }} alt={item.caption} />
                 </a>
             );
         } 
@@ -64,7 +63,11 @@ const InstaFeed = () => {
     };
     
     const thumbnailTemplate = (item) => {
-        return <img src={item.media_url} style={{ width: '40%' }} />;
+        if (item.media_type === "VIDEO") {
+            return <img src={item.thumbnail_url} className="w-28 h-28" alt={item.caption} />;
+        } else {
+            return <img src={item.media_url} style={{ width: '90%' }} alt={item.caption} />;
+        }
     };
 
     return (
