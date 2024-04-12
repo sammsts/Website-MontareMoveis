ef0fe9a24ec79dc8ced124bd67c07413a5211023
sammsts <samuelmacielsts@gmail.com>
2024-04-12 00:05:27 -0300

    Feat: Header shading

--- src/ui/components/header/Header.jsx
+++ src/ui/components/header/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white">
+    <header className="bg-white shadow-lg">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
